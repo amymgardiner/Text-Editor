c6a6ccff3ddba21feb8ffb52692f06e2e6396f62
Amy Gardiner <amymgardiner@gmail.com>
2022-09-24 18:08:04 -0500

    finished second TODO in webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add CSS loaders and babel to webpack.
+const path = require('path');
 
 module.exports = () => {
   return {
@@ -49,9 +47,28 @@ module.exports = () => {
         ],
       }),
     ],
-
+    // Add CSS loaders and babel to webpack.
     module: {
-      rules: [],
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime',
+              ],
+            },
+          },
+        },
+      ],
     },
   };
 };
