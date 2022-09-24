e837da5f6ad1f06635b0ea20e09f4dbd0360f0f4
Amy Gardiner <amymgardiner@gmail.com>
2022-09-24 18:37:07 -0500

    second TODO in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,12 +1,15 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+  // creating a new database, which will be using version 1 of the database
   openDB('jate', 1, {
+    // add database schema if it has not already been initialized
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      // create a new object store for the data and give it a key name of 'id' which will increment automatically
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
@@ -27,7 +30,21 @@ export const putDb = async (content) => {
   console.log('Data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+  // connect to DB and version we want to use
+  const jateDB = await openDB('jate', 1);
+  // create a new transaction and specify the store and data privileges
+  const tx = jateDB.transaction('jate', 'readonly');
+  // open up the desired object store
+  const store = tx.objectStore('jate');
+  // get all data in the database
+  const request = store.getAll();
+  // get confirmation of the request
+  const result = await request;
+  console.log(result);
+  return result;
+};
 
 initdb();
