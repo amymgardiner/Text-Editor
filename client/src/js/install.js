0d5b14155f6a3921e7850b278c66cf8cec31b349
Amy Gardiner <amymgardiner@gmail.com>
2022-09-24 18:54:23 -0500

    TODOs in install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,30 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  // prevent the mini-infobar from appearing on mobile
+  event.preventDefault();
+  // stash the event so it can be triggered later
+  window.deferredPrompt = event;
+  // remove the 'hidden' class from the install button container
+  butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  // show the install prompt
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  // hide the install button
+  butInstall.classList.toggle('hidden', true);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  window.deferredPrompt = null;
+});
