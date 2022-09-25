97700dfec933e64330baed5cda1925cc603a3e37
Amy Gardiner <amymgardiner@gmail.com>
2022-09-24 19:17:10 -0500

    working on package.json's

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+require('./routes/htmlRoutes.js')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
